ccd9a845df0200550fea1576b35228b8a4d0f3a1
Andy Armstrong <andyarmstro@gmail.com>
2017-08-24 14:38:37 -0700

    tweaked example

--- src/app/contact/form/form.component.ts
+++ src/app/contact/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators }   from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  public fb: FormBuilder;
+
+  public form: FormGroup;
+
+  constructor() { 
+
+    this.fb = new FormBuilder();
+
+  }
 
   ngOnInit() {
+
+  }
+
+  onSubmit() {
+
   }
 
 }
